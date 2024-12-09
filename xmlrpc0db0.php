a9f7200e17caa771f470e50d18f4b5f7d27dedf5
unknown <admin@antoniomartinezperez.com>
2024-12-09 19:02:46 +0100

    up

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -4,11 +4,11 @@
 		<engineLink>https://wordpress.org/</engineLink>
 		<homePageLink>http://localhost:8081</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="http://localhost:8081/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://localhost:8081/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://localhost:8081/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="http://localhost:8081/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="http://localhost:8081/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://antoniompdev.github.io/staticthehubofficexmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://antoniompdev.github.io/staticthehubofficexmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://antoniompdev.github.io/staticthehubofficexmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://antoniompdev.github.io/staticthehubofficexmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://antoniompdev.github.io/staticthehubofficewp-json/" />
 			</apis>
 	</service>
 </rsd>
